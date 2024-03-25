c95472ea7149f8f7a82e72361b1beb15005a9c5b
skorea6 <skorea6@naver.com>
2024-03-25 15:07:32 +0900

    Initial commit

---
+++ src/main/java/com/example/snapcampus/SnapCampusApplication.java
@@ -0,0 +1,13 @@
+package com.example.snapcampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapCampusApplication.class, args);
+	}
+
+}
