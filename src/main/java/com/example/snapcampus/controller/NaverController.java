a27ab451981aed6ba9afd2b878e6670b2a4d035e
skorea6 <skorea6@naver.com>
2024-03-25 19:57:29 +0900

    bootstrap을 이용한 템플릿 구성

--- src/main/java/com/example/snapcampus/controller/NaverController.java
+++ src/main/java/com/example/snapcampus/controller/NaverController.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class NaverController {
     //첫 페이지 호출 시, index 뷰를 호출
+    @GetMapping("/naver")
+    public String naverMap() {
+        return "naver/index";
+    }
+
     @GetMapping("/")
     public String index() {
-        return "naver/index";
+        return "/index";
+    }
+
+    @GetMapping("/search-result")
+    public String searchResult() {
+        return "search-result/index";
     }
 }
