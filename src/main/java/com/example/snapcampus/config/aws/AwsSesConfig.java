f4b5c842e30993d7da21d491ff5b2a745bec5692
skorea6 <skorea6@naver.com>
2024-04-29 13:22:20 +0900

    AwsSesConfig - 이메일 aws ses 설정

---
+++ src/main/java/com/example/snapcampus/config/aws/AwsSesConfig.java
@@ -0,0 +1,29 @@
+package com.example.snapcampus.config.aws;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsSesConfig {
+
+    @Value("${aws.ses.access_key}")
+    private String accessKey;
+
+    @Value("${aws.ses.secret_key}")
+    private String secretKey;
+
+    @Bean
+    public AmazonSimpleEmailService amazonSimpleEmailService() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        AWSStaticCredentialsProvider awsStaticCredentialsProvider = new AWSStaticCredentialsProvider(basicAWSCredentials);
+        return AmazonSimpleEmailServiceClientBuilder.standard()
+                .withCredentials(awsStaticCredentialsProvider)
+                .withRegion("ap-northeast-2")
+                .build();
+    }
+}
