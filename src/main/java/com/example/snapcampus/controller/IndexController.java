82240bb4141f1c4fc5105d11e1f319e8a40f8195
skorea6 <skorea6@naver.com>
2024-05-02 00:08:01 +0900

    IndexController - 이름 리펙토링 및 정리

--- src/main/java/com/example/snapcampus/controller/IndexController.java
+++ src/main/java/com/example/snapcampus/controller/IndexController.java
@@ -1,16 +1,13 @@
 package com.example.snapcampus.controller;
 
+import com.example.snapcampus.service.SecurityService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class NaverController {
-    //첫 페이지 호출 시, index 뷰를 호출
-    @GetMapping("/naver")
-    public String naverMap() {
-        return "naver/index";
-    }
-
+public class IndexController {
     @GetMapping("/")
     public String index() {
         return "/index";
