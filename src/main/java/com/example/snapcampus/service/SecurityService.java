63aee972b2e1cb5cda5246885d67e7d916ef8d0d
skorea6 <skorea6@naver.com>
2024-05-01 23:38:23 +0900

    SecurityService - authentication, principal, userId 쉽게 가져올 수 있도록 설정

---
+++ src/main/java/com/example/snapcampus/service/SecurityService.java
@@ -0,0 +1,47 @@
+package com.example.snapcampus.service;
+
+import com.example.snapcampus.dto.security.SnapPrincipal;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class SecurityService {
+
+    private Object getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    public SnapPrincipal getPrincipal() {
+        Object principal = getAuthentication();
+        if (principal instanceof SnapPrincipal) {
+            return ((SnapPrincipal) principal);
+        } else {
+            return null;
+        }
+    }
+
+
+    public String getMemberUserId() {
+        SnapPrincipal principal = getPrincipal();
+        if (principal != null) {
+            return principal.getUsername();
+        } else {
+            return null;
+        }
+    }
+
+    public List<String> getAuthorities() {
+        SnapPrincipal principal = getPrincipal();
+        if (principal != null) {
+            return principal.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .collect(Collectors.toList());
+        } else {
+            return null;
+        }
+    }
+}
