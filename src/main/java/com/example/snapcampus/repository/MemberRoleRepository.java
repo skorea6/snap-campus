a817104f92ba3f2dde67fd4452c61861eace01b4
skorea6 <skorea6@naver.com>
2024-05-01 23:35:21 +0900

    MemberRoleRepository 생성

---
+++ src/main/java/com/example/snapcampus/repository/MemberRoleRepository.java
@@ -0,0 +1,8 @@
+package com.example.snapcampus.repository;
+
+import com.example.snapcampus.entity.Member;
+import com.example.snapcampus.entity.MemberRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRoleRepository extends JpaRepository<MemberRole, String> {
+}
