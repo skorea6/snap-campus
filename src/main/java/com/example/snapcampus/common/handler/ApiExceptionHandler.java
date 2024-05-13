d94659278918e0a7832a6bc67d912187a30af2ac
skorea6 <skorea6@naver.com>
2024-05-13 15:02:13 +0900

    API 작업 - ApiExceptionHandler 추가

---
+++ src/main/java/com/example/snapcampus/common/handler/ApiExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.example.snapcampus.common.handler;
+
+import com.example.snapcampus.common.dto.BaseResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BaseResponse<String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+//        Map<String, String> errors = new HashMap<>();
+//        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+//            errors.put(error.getField(), error.getDefaultMessage());
+//        }
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        String defaultMessage = fieldErrors.get(0).getDefaultMessage();
+        BaseResponse<String> response = new BaseResponse<>(defaultMessage);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    // 모든 기타 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<BaseResponse<String>> handleAllExceptions(Exception ex) {
+        BaseResponse<String> response = new BaseResponse<>("서버 오류가 발생했습니다.");
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
