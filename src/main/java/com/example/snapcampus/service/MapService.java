9e43b8b597d09490ab551d4d7c4010fc310e9d86
skorea6 <skorea6@naver.com>
2024-05-20 19:55:17 +0900

    MapService - getAllPlace 메소드 추가

--- src/main/java/com/example/snapcampus/service/MapService.java
+++ src/main/java/com/example/snapcampus/service/MapService.java
@@ -1,11 +1,14 @@
 package com.example.snapcampus.service;
 
 import com.example.snapcampus.dto.request.map.MapPlaceAddRequest;
+import com.example.snapcampus.dto.response.map.MapResponse;
 import com.example.snapcampus.entity.Map;
 import com.example.snapcampus.repository.MapRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -13,6 +16,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class MapService {
     private final MapRepository mapRepository;
+
+    public List<MapResponse> getAllPlace(){
+        List<Map> maps = mapRepository.findAll();
+        return maps.stream().map(Map::toDto).toList();
+    }
 
     public String addPlace(MapPlaceAddRequest mapPlaceAddRequest){
         // TODO: 이름 중복 검사 + x, y 좌표 중복 검사 (시간되면)
