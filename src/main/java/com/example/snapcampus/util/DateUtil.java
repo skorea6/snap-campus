b34df9f6deb4df0583ca2db598482819ac13bbf1
skorea6 <skorea6@naver.com>
2024-07-02 14:31:54 +0900

    DateUtil - format 추가

--- src/main/java/com/example/snapcampus/util/DateUtil.java
+++ src/main/java/com/example/snapcampus/util/DateUtil.java
@@ -3,9 +3,27 @@ package com.example.snapcampus.util;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class DateUtil {
     public static String formatDateTime(LocalDateTime date) {
         return date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    public static String formatDate(LocalDate date) {
+        return date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+
+    public static LocalDate stringToLocalDate(String date){
+        LocalDate returnDate = null;
+        if(date != null){
+            try {
+                returnDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            } catch (DateTimeParseException e) {
+                throw new IllegalArgumentException("date format이 올바르지 않습니다.");
+            }
+        }
+
+        return returnDate;
     }
 }
