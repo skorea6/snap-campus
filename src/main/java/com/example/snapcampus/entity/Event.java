cc839ad81edefcc89f83996662a2f409d9fe894b
skorea6 <skorea6@naver.com>
2024-07-02 14:34:53 +0900

    Event - member, map, posts, toDto() 추가

--- src/main/java/com/example/snapcampus/entity/Event.java
+++ src/main/java/com/example/snapcampus/entity/Event.java
@@ -1,10 +1,19 @@
 package com.example.snapcampus.entity;
 
+import com.example.snapcampus.dto.response.event.EventDtoResponse;
+import com.example.snapcampus.util.DateUtil;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Getter
 @ToString(callSuper = true) // auditing 필드까지 호출
@@ -25,12 +34,47 @@ public class Event extends AuditingFields{
     private String description;
 
     @Column
-    private LocalDateTime startDate;
+    private LocalDate startDate;
 
     @Column
-    private LocalDateTime stopDate;
+    private LocalDate stopDate;
 
     @Column
     private String organizer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", foreignKey = @ForeignKey(name = "fk_event_member_id"))
+    @Setter
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "map_id", foreignKey = @ForeignKey(name = "fk_event_map_id"))
+    @Setter
+    private Map map;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "event", cascade = {CascadeType.ALL}, targetEntity = Post.class)
+    private List<Post> posts = new ArrayList<>();
+
+    public Event(String name, String description, LocalDate startDate, LocalDate stopDate, String organizer) {
+        this.name = name;
+        this.description = description;
+        this.startDate = startDate;
+        this.stopDate = stopDate;
+        this.organizer = organizer;
+    }
+
+    public Event() {
+
+    }
+
+    public EventDtoResponse toDto(){
+        AtomicLong counter = new AtomicLong(1);
+
+        return new EventDtoResponse(id, name, description, DateUtil.formatDate(startDate), DateUtil.formatDate(stopDate), organizer, member.toDto(), posts.stream().map(post -> {
+            long currentIndex = counter.getAndIncrement();
+            currentIndex = (currentIndex - 1) % 4 + 1;
+            return post.toDto(currentIndex);
+        }).toList());
+    }
 }
 
