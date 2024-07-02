5b499cfd0554368018dea18bc1971869df4fc3e5
skorea6 <skorea6@naver.com>
2024-07-02 14:35:22 +0900

    MapDtoResponse에 member, events 필드 추가

--- src/main/java/com/example/snapcampus/dto/response/map/MapDtoResponse.java
+++ src/main/java/com/example/snapcampus/dto/response/map/MapDtoResponse.java
@@ -1,8 +1,13 @@
 package com.example.snapcampus.dto.response.map;
 
 
+import com.example.snapcampus.dto.response.event.EventDtoResponse;
+import com.example.snapcampus.dto.response.member.MemberDtoResponse;
+import com.example.snapcampus.dto.response.post.PostDtoResponse;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -14,12 +19,17 @@ public class MapDtoResponse {
     private String placeType;
     private String placeDescription;
 
-    public MapDtoResponse(Long id, Double coordinate_x, Double coordinate_y, String placeName, String placeType, String placeDescription) {
+    private MemberDtoResponse member;
+    private List<EventDtoResponse> events;
+
+    public MapDtoResponse(Long id, Double coordinate_x, Double coordinate_y, String placeName, String placeType, String placeDescription, MemberDtoResponse member, List<EventDtoResponse> events) {
         this.id = id;
         this.coordinate_x = coordinate_x;
         this.coordinate_y = coordinate_y;
         this.placeName = placeName;
         this.placeType = placeType;
         this.placeDescription = placeDescription;
+        this.member = member;
+        this.events = events;
     }
 }
