fbd8fea9e46a319e6430267dfeeb56eee9cf4f17
skorea6 <skorea6@naver.com>
2024-07-02 14:34:34 +0900

    Post - images, likeCount, toDto() 추가

--- src/main/java/com/example/snapcampus/entity/Post.java
+++ src/main/java/com/example/snapcampus/entity/Post.java
@@ -1,7 +1,9 @@
 package com.example.snapcampus.entity;
 
+import com.example.snapcampus.dto.response.post.PostDtoResponse;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.util.List;
@@ -21,25 +23,47 @@ public class Post extends AuditingFields{
     // Post 여러개 하나의 회원 Member
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", foreignKey = @ForeignKey(name = "fk_post_member_id"))
+    @ToString.Exclude
+    @Setter
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "map_id", foreignKey = @ForeignKey(name = "fk_post_map_id"))
+    @ToString.Exclude
+    @Setter
     private Map map;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "event_id", foreignKey = @ForeignKey(name = "fk_post_event_id"))
+    @ToString.Exclude
+    @Setter
     private Event event;
+
+//    @Column(nullable = false)
+//    private String title;
 
     @Column(nullable = false)
     private String description;
 
     @ElementCollection(fetch = FetchType.LAZY)
-    private List<String> image;
+    private List<String> images;
 
     @Column(nullable = false)
-    private String likeCount;
+    private Long likeCount = 0L;
 
     @Column(nullable = false)
     private String department;
+
+    public Post(String description, String department) {
+        this.description = description;
+        this.department = department;
+    }
+
+    public Post() {
+
+    }
+
+    public PostDtoResponse toDto(Long columnIndex){
+        return new PostDtoResponse(id, description, likeCount, department, images, columnIndex, member.toDto());
+    }
 }
