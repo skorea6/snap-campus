1668c0f108a81c784e1ba3f2cef3ff7abed0189a
skorea6 <skorea6@naver.com>
2024-07-04 01:15:37 +0900

    Post response dto 추가

---
+++ src/main/java/com/example/snapcampus/dto/response/post/PostDtoResponse.java
@@ -0,0 +1,38 @@
+package com.example.snapcampus.dto.response.post;
+
+
+import com.example.snapcampus.dto.response.event.EventDtoResponse;
+import com.example.snapcampus.dto.response.member.MemberDtoResponse;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class PostDtoResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private Long likeCount;
+    private String department;
+    private String createdAt;
+    private String thumbImage;
+    private List<String> images;
+
+    private Long columnIndex;
+    private MemberDtoResponse member;
+
+    public PostDtoResponse(Long id, String title, String description, Long likeCount, String department, String createdAt, String thumbImage, List<String> images, Long columnIndex, MemberDtoResponse member) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.likeCount = likeCount;
+        this.department = department;
+        this.createdAt = createdAt;
+        this.thumbImage = thumbImage;
+        this.images = images;
+        this.columnIndex = columnIndex;
+        this.member = member;
+    }
+}
