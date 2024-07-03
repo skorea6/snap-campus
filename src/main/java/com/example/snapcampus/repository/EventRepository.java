b42bc1b6c9ad477e016acfd63d4dd7a1b2bea509
skorea6 <skorea6@naver.com>
2024-07-04 01:06:09 +0900

    Post, Event Repository 추가

--- src/main/java/com/example/snapcampus/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.snapcampus.repository;
-
-import com.example.snapcampus.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
