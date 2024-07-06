f637b95db9f8f9c128a88a00c50662e65edb30b9
skorea6 <skorea6@naver.com>
2024-07-06 15:57:39 +0900

    Post request Dto - 이름 변경 (dto추가)

--- src/main/java/com/example/snapcampus/dto/request/post/PostAddDtoRequest.java
+++ src/main/java/com/example/snapcampus/dto/request/post/PostAddDtoRequest.java
@@ -1,12 +1,8 @@
 package com.example.snapcampus.dto.request.post;
 
-import com.example.snapcampus.entity.Event;
 import com.example.snapcampus.entity.Post;
-import com.example.snapcampus.util.DateUtil;
-import com.example.snapcampus.util.PatternUtil;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,7 +12,7 @@ import java.util.List;
 
 @Getter
 @Setter
-public class PostAddRequest {
+public class PostAddDtoRequest {
     @NotNull(message = "eventId는 필수 입력 값입니다.")
     private Long eventId;
 
@@ -25,12 +21,13 @@ public class PostAddRequest {
     private String title;
 
     @NotBlank(message = "사진 설명은 필수 입력 값입니다.")
-    @Size(max = 200, message = "이벤트 설명은 최대 200자여야 합니다.")
+    @Size(max = 200, message = "사진 설명은 최대 200자여야 합니다.")
     private String description;
 
     @Size(max = 20, message = "과는 최대 20자여야 합니다.")
     private String department;
 
+    @NotNull(message = "사진을 최소 1장 추가해주세요.")
     private List<MultipartFile> images;
 
     public Post toEntity(){
