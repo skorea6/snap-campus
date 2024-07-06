29a7cc3748dd03c9b67b04cf8a022677f1e72bcc
skorea6 <skorea6@naver.com>
2024-07-06 16:08:05 +0900

    MemberRole - DB 수동관리를 위해 GenerationType.IDENTITY로 변경

--- src/main/java/com/example/snapcampus/entity/MemberRole.java
+++ src/main/java/com/example/snapcampus/entity/MemberRole.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 @Getter
 public class MemberRole {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 30)
