e5d6a7b1f38ebd68c35ea1e72681b6f7cdf11f84
skorea6 <skorea6@naver.com>
2024-07-06 16:09:21 +0900

    api 에러 예외처리 및 errorMessage 뷰에 넘기기

--- src/main/java/com/example/snapcampus/controller/CustomErrorController.java
+++ src/main/java/com/example/snapcampus/controller/CustomErrorController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CustomErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        String requestUri = (String) request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
+        Object errorMessage = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
 
-        if (status != null) {
+        if (status != null && !requestUri.startsWith("/api/")) {
             int statusCode = Integer.parseInt(status.toString());
 
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
@@ -23,6 +26,11 @@ public class CustomErrorController implements ErrorController {
             }else if(statusCode == HttpStatus.FORBIDDEN.value()){
                 return "redirect:/";
             }
+        }
+
+        // 에러 메시지가 있다면 모델에 추가
+        if (errorMessage != null) {
+            model.addAttribute("errorMsg", errorMessage.toString());
         }
 
         return "/error/default";  // 일반 에러 페이지로 이동
