80d5b08ac53d5046d52757561c30947c89b2f57c
skorea6 <skorea6@naver.com>
2024-07-07 14:42:49 +0900

    MapService - 삭제 메소드 추가 및 dto 이름 변경 적용

--- src/main/java/com/example/snapcampus/service/MapService.java
+++ src/main/java/com/example/snapcampus/service/MapService.java
@@ -1,6 +1,7 @@
 package com.example.snapcampus.service;
 
-import com.example.snapcampus.dto.request.map.MapPlaceAddRequest;
+import com.example.snapcampus.dto.request.map.MapPlaceAddDtoRequest;
+import com.example.snapcampus.dto.request.map.MapPlaceDeleteDtoRequest;
 import com.example.snapcampus.dto.response.map.MapAddDtoResponse;
 import com.example.snapcampus.dto.response.map.MapDetailDtoResponse;
 import com.example.snapcampus.dto.response.map.MapDtoResponse;
@@ -20,15 +21,17 @@ import java.util.List;
 public class MapService {
     private final MapRepository mapRepository;
     private final MemberRepository memberRepository;
+    private final AwsS3Service awsS3Service;
+
 
     public List<MapDtoResponse> getAllPlace(){
         List<Map> maps = mapRepository.findAll();
         return maps.stream().map(Map::toDto).toList();
     }
 
-    public MapAddDtoResponse addPlace(String memberUserId, MapPlaceAddRequest mapPlaceAddRequest){
+    public MapAddDtoResponse addPlace(String memberUserId, MapPlaceAddDtoRequest mapPlaceAddDtoRequest){
         // TODO: 이름 중복 검사 + x, y 좌표 중복 검사 (시간되면)
-        Map entity = mapPlaceAddRequest.toEntity();
+        Map entity = mapPlaceAddDtoRequest.toEntity();
         entity.setMember(memberRepository.findByUserId(memberUserId));
         Map savedMap = mapRepository.save(entity);
 
@@ -41,5 +44,21 @@ public class MapService {
         );
 
         return map.toDetailDto();
+    }
+
+    public String deletePlace(MapPlaceDeleteDtoRequest mapPlaceDeleteDtoRequest) {
+        Map map = mapRepository.findById(mapPlaceDeleteDtoRequest.getId()).orElseThrow(
+                () -> new IllegalArgumentException("찾을 수 없는 id입니다.")
+        );
+
+        // 현재는 Admin만 삭제하게끔 허용하기 때문에 작성자와 로그인 유저가 일치하는지 확인하는 과정은 필요 없음
+        map.getEvents().forEach(
+                e -> e.getPosts().forEach(
+                        p -> p.getImages().forEach(awsS3Service::deleteFile)
+                )
+        ); // 기존 이미지 파일 삭제 (s3)
+
+        mapRepository.deleteById(map.getId());
+        return "장소가 삭제되었습니다.";
     }
 }
