ffad9e8a60def7a89db55222b32cf9863cc12288
skorea6 <skorea6@naver.com>
2024-07-07 14:44:56 +0900

    Map delete 컨트롤러 mapping 추가 및 dto 이름 변경

--- src/main/java/com/example/snapcampus/controller/api/MapApiController.java
+++ src/main/java/com/example/snapcampus/controller/api/MapApiController.java
@@ -2,7 +2,8 @@ package com.example.snapcampus.controller.api;
 
 
 import com.example.snapcampus.common.dto.BaseResponse;
-import com.example.snapcampus.dto.request.map.MapPlaceAddRequest;
+import com.example.snapcampus.dto.request.map.MapPlaceAddDtoRequest;
+import com.example.snapcampus.dto.request.map.MapPlaceDeleteDtoRequest;
 import com.example.snapcampus.dto.response.map.MapAddDtoResponse;
 import com.example.snapcampus.service.MapService;
 import com.example.snapcampus.service.SecurityService;
@@ -21,11 +22,17 @@ public class MapApiController {
     }
 
     @PostMapping("/place/add")
-    public BaseResponse<MapAddDtoResponse> addPlace(@RequestBody @Valid MapPlaceAddRequest mapPlaceAddRequest) {
+    public BaseResponse<MapAddDtoResponse> addPlace(@RequestBody @Valid MapPlaceAddDtoRequest mapPlaceAddDtoRequest) {
         securityService.checkIsLogined();
 
         String memberUserId = securityService.getMemberUserId();
-        MapAddDtoResponse response = mapService.addPlace(memberUserId, mapPlaceAddRequest);
+        MapAddDtoResponse response = mapService.addPlace(memberUserId, mapPlaceAddDtoRequest);
         return new BaseResponse<>(response);
+    }
+
+    @PostMapping("/place/delete")
+    public BaseResponse<Object> deletePlace(@RequestBody @Valid MapPlaceDeleteDtoRequest mapPlaceDeleteDtoRequest) {
+        String resultMsg = mapService.deletePlace(mapPlaceDeleteDtoRequest);
+        return new BaseResponse<>(resultMsg);
     }
 }
