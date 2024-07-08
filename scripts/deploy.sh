a9c98ec70f9c388f5cd5ea7246ad81412cef02e6
skorea6 <skorea6@naver.com>
2024-07-08 17:37:04 +0900

    deploy.sh - sudo 빼고 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ LOG_FILE=./deploy.log
 
 # 실행중인 blue가 있는지 확인
 # 프로젝트의 실행 중인 컨테이너를 확인하고, 해당 컨테이너가 실행 중인지 여부를 EXIST_BLUE 변수에 저장
-EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
 # 배포 시작한 날짜와 시간을 기록
 echo "배포 시작일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $LOG_FILE
@@ -23,37 +23,37 @@ if [ -z "$EXIST_BLUE" ]; then
   echo "blue 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $LOG_FILE
 
 	# docker-compose.blue.yml 파일을 사용하여 spring-blue 프로젝트의 컨테이너를 빌드하고 실행
-	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
 
   # 30초 동안 대기
   sleep 30
 
   # 8112(green)을 8111(blue)로 변경 (nginx proxy manager)
-#  sudo /bin/bash -c "sed -i 's/8112/8111/' ${NGINX_CONFIG_URL} && nginx -s reload"
+#  /bin/bash -c "sed -i 's/8112/8111/' ${NGINX_CONFIG_URL} && nginx -s reload"
 
   # /home/ubuntu/deploy.log: 로그 파일에 "green 중단 시작"이라는 내용을 추가
   echo "green 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $LOG_FILE
 
   # docker-compose.green.yml 파일을 사용하여 spring-green 프로젝트의 컨테이너를 중지
-  sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+  docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 
    # 사용하지 않는 이미지 삭제
-  sudo docker image prune -af
+  docker image prune -af
 
   echo "green 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $LOG_FILE
 
 # blue가 실행중이면 green up
 else
 	echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $LOG_FILE
-	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
 
   sleep 30
 
-#  sudo /bin/bash -c "sed -i 's/8111/8112/' ${NGINX_CONFIG_URL} && nginx -s reload"
+#  /bin/bash -c "sed -i 's/8111/8112/' ${NGINX_CONFIG_URL} && nginx -s reload"
 
   echo "blue 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $LOG_FILE
-  sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
-  sudo docker image prune -af
+  docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+  docker image prune -af
 
   echo "blue 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $LOG_FILE
 
