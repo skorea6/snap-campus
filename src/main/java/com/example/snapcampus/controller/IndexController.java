bcc85200207a6047c5cf691402f02b9964a442a5
skorea6 <skorea6@naver.com>
2024-07-14 13:17:46 +0900

    메인페이지에 로그인했을 경우 내가 좋아요 누른 포스트 보여주기

--- src/main/java/com/example/snapcampus/controller/IndexController.java
+++ src/main/java/com/example/snapcampus/controller/IndexController.java
@@ -4,7 +4,9 @@ import com.example.snapcampus.dto.response.event.EventDetailDtoResponse;
 import com.example.snapcampus.dto.response.map.MapDtoResponse;
 import com.example.snapcampus.dto.response.post.PostDetailDtoResponse;
 import com.example.snapcampus.service.MapService;
+import com.example.snapcampus.service.PostLikeService;
 import com.example.snapcampus.service.PostService;
+import com.example.snapcampus.service.SecurityService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,14 +18,20 @@ import java.util.List;
 public class IndexController {
     private final MapService mapService;
     private final PostService postService;
+    private final PostLikeService postLikeService;
+    private final SecurityService securityService;
 
-    public IndexController(MapService mapService, PostService postService) {
+    public IndexController(MapService mapService, PostService postService, PostLikeService postLikeService, SecurityService securityService) {
         this.mapService = mapService;
         this.postService = postService;
+        this.postLikeService = postLikeService;
+        this.securityService = securityService;
     }
 
     @GetMapping("/")
     public String index(Model model) {
+        String memberUserId = securityService.getMemberUserId();
+
         List<MapDtoResponse> places = mapService.getAllPlace();
         List<PostDetailDtoResponse> allPosts = postService.getAllPosts();
 
@@ -36,6 +44,13 @@ public class IndexController {
 
         model.addAttribute("places", places);
         model.addAttribute("allColumnPosts", columnPosts);
+
+        if(memberUserId != null){
+            List<PostDetailDtoResponse> likePosts = postLikeService.list(memberUserId);
+            model.addAttribute("likePosts", likePosts);
+            model.addAttribute("hasLikePosts", true);
+        }
+
         return "index";
     }
 
