08fca4494960d4d4ad2a3b94b52c76bb2fb738bc
skorea6 <skorea6@naver.com>
2024-08-22 01:21:43 +0900

    Revert "SecurityConfig에 jwt 필터 추가"
    This reverts commit 7a9df61c310a1fc9641fa2dcea581c26739bb59e.

--- src/main/java/com/example/snapcampus/config/SecurityConfig.java
+++ src/main/java/com/example/snapcampus/config/SecurityConfig.java
@@ -1,16 +1,11 @@
 package com.example.snapcampus.config;
 
 import com.example.snapcampus.common.handler.CustomAuthenticationFailureHandler;
-import com.example.snapcampus.common.login.jwt.JwtAuthenticationFilter;
-import com.example.snapcampus.common.login.jwt.JwtTokenProvider;
 import com.example.snapcampus.dto.security.SnapPrincipal;
-import com.example.snapcampus.repository.MemberRepository;
 import com.example.snapcampus.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
@@ -19,19 +14,12 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-
-import java.util.Optional;
-
 @Configuration
 public class SecurityConfig {
 
     @Autowired
     private CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
-
-    @Autowired
-    private JwtTokenProvider jwtTokenProvider;
 
     private static final String[] requestPermitStaticUrl = new String[]{
             "/css/**",
@@ -59,7 +47,7 @@ public class SecurityConfig {
                         .requestMatchers(requestPermitAllUrl)
                         .anonymous()
                         .requestMatchers("/api/map/place/delete", "/api/event/delete").hasRole("ADMIN")
-                        .requestMatchers("/member/**", "/api/map/place/add").hasRole("MEMBER")
+                        .requestMatchers("/member/**").hasRole("MEMBER")
                         .anyRequest().permitAll()
                 )
                 .formLogin(form -> form
@@ -78,10 +66,6 @@ public class SecurityConfig {
                         .deleteCookies("JSESSIONID")  // 쿠키 삭제
                         .permitAll()
                 )
-                .addFilterBefore(
-                        new JwtAuthenticationFilter(jwtTokenProvider), // 먼저 실행 (앞에 있는 필터가 통과하면 뒤에 있는 필터는 검사하지 않음)
-                        UsernamePasswordAuthenticationFilter.class
-                )
 //                .exceptionHandling(
 //                        e -> e.accessDeniedHandler(new CustomAccessDeniedHandler())
 //                )
@@ -89,20 +73,20 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(MemberRepository memberRepository) {
-        return username -> Optional.ofNullable(memberRepository.findByUserId(username))
+    public UserDetailsService userDetailsService(MemberService memberService) {
+        return username -> memberService
+                .searchUser(username)
                 .map(SnapPrincipal::from)
                 .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+//    @Bean
+//    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+//        return new GrantedAuthorityDefaults(""); // 역할 접두사를 제거
+//    }
 }
